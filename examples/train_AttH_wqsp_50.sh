f3ce9b48d88c06f0fd33cedbd1fcd8dc0f41447b
Nadya Abdel Madjid <nadya.abdelmadjid@gmail.com>
2022-08-29 14:32:20 +0400

    Update train_AttH_wqsp_50.sh

--- examples/train_AttH_wqsp_50.sh
+++ examples/train_AttH_wqsp_50.sh
@@ -1,1 +1,12 @@
-python main.py --dataset wqsp --model AttH --dim 400 --kg_type half --valid_every 10 --max_epochs 50 --learning_rate 0.00002 --qa_nn_type SBERT --batch_size 16 --checkpoint_type libkge --freeze True --use_relation_matching True --rel_gamma 5.5
+#!/bin/bash
+python main.py \
+            --dataset wqsp \
+            --model AttH \ 
+            --dim 50 \
+            --kg_type half \
+            --valid_every 10 \ 
+            --max_epochs 50 \ 
+            --learning_rate 0.00002 \
+            --batch_size 16 \
+            --checkpoint_type libkge \ 
+            --freeze True \ 
