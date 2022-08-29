7954ba2c9939f60e74dc00574c9cb912933d0ab2
Nadya Abdel Madjid <nadya.abdelmadjid@gmail.com>
2022-08-29 12:48:59 +0400

    Update train_AttH_MetaQA_50.sh

--- examples/train_AttH_MetaQA_50.sh
+++ examples/train_AttH_MetaQA_50.sh
@@ -1,1 +1,11 @@
-train_ComplEx_MetaQA_50.sh
+#!/bin/bash
+python main.py \
+            --dataset MetaQA \
+            --model AttH \ 
+            --dim 400 \
+            --kg_type full \
+            --valid_every 10 \
+            --max_epochs 50 \
+            --learning_rate 0.0005 \
+            --checkpoint_type ldh \
+            --hops 3
