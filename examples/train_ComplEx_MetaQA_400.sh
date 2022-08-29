9ca6d777ffb33105e859cdf1d380ac1283eb3495
Nadya Abdel Madjid <nadya.abdelmadjid@gmail.com>
2022-08-29 12:46:21 +0400

    Create train_ComplEx_MetaQA_400.sh

---
+++ examples/train_ComplEx_MetaQA_400.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python main.py \
+            --dataset MetaQA \
+            --model ComplEx \ 
+            --dim 400 \
+            --kg_type half \
+            --valid_every 10 \
+            --max_epochs 50 \
+            --learning_rate 0.0005 \
+            --checkpoint_type ldh \
