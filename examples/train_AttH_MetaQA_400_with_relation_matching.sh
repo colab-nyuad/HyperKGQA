9886a5c52b6a6773cd96bf958610da5015399779
Nadya Abdel Madjid <nadya.abdelmadjid@gmail.com>
2022-08-29 14:37:40 +0400

    Create train_AttH_MetaQA_400_with_relation_matching.sh

---
+++ examples/train_AttH_MetaQA_400_with_relation_matching.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python main.py \
+            --dataset MetaQA \
+            --model AttH \ 
+            --dim 400 \
+            --kg_type full \
+            --valid_every 10 \
+            --max_epochs 50 \
+            --learning_rate 0.0005 \
+            --checkpoint_type ldh \
+            --hops 3 \
+            --use_relation_matching True \
+            --rel_gamma 7.5 \
